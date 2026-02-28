46ce4440971ac07a6a1deafb7ad19952d71d2a4b
eduard <egrueso@unal.edu.co>
2026-02-27 21:59:29 -0500

    test program

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+projectyDir=$1
+projectyDir=${projectyDir%/} # Eliminar la barra final si existe
+
+#verificar si se ha proporcionado un argumento
+if [ -z "$projectyDir" ]; then
+    echo "Uso: $0 <directorio_del_proyecto>"
+    exit 1
+fi
+
+#verificar si el directorio del proyecto existe
+if [ ! -d "$projectyDir" ]; then
+    echo "El directorio del proyecto no existe."
+    exit 1
+fi
+
+# Verificar si el directorio de build existe
+if [ -e "$projectyDir/build" ]; then
+    echo "El directorio de build ya existe. Eliminando..."
+    rm -rf "$projectyDir/build"
+    mkdir -p "$projectyDir/build"
+else
+    echo "El directorio de build no existe. Creando..."
+    mkdir -p "$projectyDir/build"
+fi
+
+# Cambiar al directorio de build
+cd "$projectyDir/build"
+
+# Ejecutar CMake para configurar el proyecto
+cmake ..
+cmake --build . --config Release
+
+exit 0
